d0471ff2ab9952fcc7eab618aad4f66691c6e43b
Vineet Raj <vineetraj14@gmail.com>
2017-01-21 03:15:38 +0530

    Change layout to Tabbed. Add ViewPager And FragmentPagerAdapter. Add Visual polish to app.

---
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -0,0 +1,68 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * {@link CategoryAdapter} is a {@link FragmentPagerAdapter} that can provide the layout for
+ * each list item based on a data source which is a list of {@link Word} objects.
+ */
+public class CategoryAdapter extends FragmentPagerAdapter {
+    private String tabTitles[] = new String[]{"Numbers", "Family", "Colors", "Phrases"};
+    private Context context;
+
+    /**
+     * Create a new {@link CategoryAdapter} object.
+     *
+     * @param fm is the fragment manager that will keep each fragment's state in the adapter
+     *           across swipes.
+     */
+    public CategoryAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new NumbersFragment();
+        } else if (position == 1) {
+            return new FamilyFragment();
+        } else if (position == 2) {
+            return new ColorsFragment();
+        } else
+            return new PhrasesFragment();
+    }
+
+//    @Override
+//    public CharSequence getPageTitle(int position) {
+//        if (position == 0) {
+//            return context.getString(R.string.category_numbers);
+//        } else if (position == 1) {
+//            return context.getString(R.string.category_family);
+//        } else if (position == 2) {
+//            return context.getString(R.string.category_colors);
+//        } else {
+//            return context.getString(R.string.category_phrases);
+//        }
+//    }
+
+    /**
+     * Return the total number of pages.
+     */
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
